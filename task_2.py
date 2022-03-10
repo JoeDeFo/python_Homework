9e831351a50f87b8c97e5ba2eb48b1b9c6b44c35
Anton <am110201@yandex.ru>
2022-03-10 17:09:00 +0300

    Homework_5_maths.stat.

--- task_2.py
+++ task_2.py
@@ -1,13 +1,27 @@
-"""В первом ящике находится 8 мячей, из которых 5 - белые. Во втором ящике - 12 мячей, из которых 5 белых.
-Из первого ящика вытаскивают случайным образом два мяча, из второго - 4.
-Какова вероятность того, что 3 мяча белые?"""
-
+"""
+В результате 10 независимых измерений некоторой величины X, выполненных с одинаковой точностью,
+получены опытные данные:
+6.9, 6.1, 6.2, 6.8, 7.5, 6.3, 6.4, 6.9, 6.7, 6.1
+Предполагая, что результаты измерений подчинены нормальному закону распределения вероятностей,
+оценить истинное значение величины X при помощи доверительного интервала, покрывающего это
+значение с доверительной вероятностью 0,95.
+"""
+import math
 import numpy as np
 from math import factorial
-def combinations(n, k):
-    return int(factorial(n) / (factorial(k) * factorial(n - k)))
 
-p(1) = ((combinations(5,2) * combinations(3,0)) / combinations(8,2)) * ((combinations(5,1) * combinations(7,3) / combinations(12,4))
-p(2) = ((combinations(5,1) * combinations(3,1)) / combinations(8,2)) * ((combinations(5,2) * combinations(7,2) / combinations(12,4))
-p(3) = ((combinations(5,0) * combinations(3,2)) / combinations(8,2)) * ((combinations(5,3) * combinations(7,1) / combinations(12,4))
-p(x) = p(1) + p(2) + p(3)
+#Дано
+the_value_of_salaries = np.array([6.9, 6.1, 6.2, 6.8, 7.5, 6.3, 6.4, 6.9, 6.7, 6.1])
+n = 10
+t(a/2)(по табличному значению) = 1.96
+
+#Найдем среднюю выборочную и дисперсию
+
+1."""Средняя выборочная""" = 6.59
+the_value_of_salaries_arithmetic_mean = the_value_of_salaries.sum() / the_value_of_salaries.size
+2."""Дисперсия""" = 0.2032
+the_value_of_salaries_dispersion = np.sum((the_value_of_salaries - the_value_of_salaries_arithmetic_mean)**2) / (the_value_of_salaries.size - 1)
+#Найдем критерий Стьюдента
+t1 = the_value_of_salaries_arithmetic_mean - (t(a/2) * math.sqrt(the_value_of_salaries_dispersion / 10)) = 6.3107
+t2 = the_value_of_salaries_arithmetic_mean + (t(a/2) * math.sqrt(the_value_of_salaries_dispersion / 10)) = 6.8693
+Ответ: [6.3107,6.8693]
